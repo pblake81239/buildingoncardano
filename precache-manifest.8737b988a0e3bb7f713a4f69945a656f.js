d067b6d6baa85ea9c49b02f39a2a325eaa21e3fb
paddy blake <paddyblake09@hotmail.com>
2021-05-20 20:19:35 +0100

    Updates

---
+++ precache-manifest.8737b988a0e3bb7f713a4f69945a656f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce8b569b0fe2fa1a51e78abbb86a8b5b",
+    "url": "https://reduction-admin.github.io/react-reduction/index.html"
+  },
+  {
+    "revision": "a3106eb214532d3a419f",
+    "url": "https://reduction-admin.github.io/react-reduction/static/css/main.6a7cf092.chunk.css"
+  },
+  {
+    "revision": "63f1d0b243bcda82df09",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/2.17683427.chunk.js"
+  },
+  {
+    "revision": "a3106eb214532d3a419f",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/main.8eb6a7ba.chunk.js"
+  },
+  {
+    "revision": "ebacf028a0bb3d438457",
+    "url": "https://reduction-admin.github.io/react-reduction/static/js/runtime~main.f524c1eb.js"
+  },
+  {
+    "revision": "978e51b554db00a67e2796661b3526fe",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/100_4.978e51b5.jpg"
+  },
+  {
+    "revision": "3cd9c77a99fd1899c50bdf701ec9f275",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/logo_200.3cd9c77a.png"
+  },
+  {
+    "revision": "f7f5fc61b5a82360ce50a2af91ae66ab",
+    "url": "https://reduction-admin.github.io/react-reduction/static/media/rsz_1sidebar-4.f7f5fc61.jpg"
+  }
+]);
